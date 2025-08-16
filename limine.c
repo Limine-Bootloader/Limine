572f32871606faa17304d8f18dc04d24375ba3c4
Mintsuki <mintsuki@protonmail.com>
2025-08-16 10:55:29 +0000

    Binary release v9.6.1

--- limine.c
+++ limine.c
@@ -1088,6 +1088,11 @@ part_too_low:
                 goto cleanup;
             }
 
+            if (((ENDSWAP(gpt_entry.ending_lba) - ENDSWAP(gpt_entry.starting_lba)) + 1) * lb_size < 32768) {
+                fprintf(stderr, "%s: error: Partition with index `%s` is smaller than 32KiB.\n", program_name, part_ndx);
+                goto cleanup;
+            }
+
             if (!quiet) {
                 fprintf(stderr, "GPT partition specified. Installing there instead of embedding.\n");
             }
@@ -1386,7 +1391,7 @@ cleanup:
     return ret;
 }
 
-#define LIMINE_VERSION "9.6.0"
+#define LIMINE_VERSION "9.6.1"
 #define LIMINE_COPYRIGHT "Copyright (C) 2019-2025 Mintsuki and contributors."
 
 static void version_usage(void) {
