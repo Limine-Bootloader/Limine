258a5f6afcc71ea6fec392d7ceb84325472dc3e0
Mintsuki <mintsuki@protonmail.com>
2025-08-30 15:01:16 +0000

    Binary release v9.6.4

--- limine.c
+++ limine.c
@@ -1391,7 +1391,7 @@ cleanup:
     return ret;
 }
 
-#define LIMINE_VERSION "9.6.3"
+#define LIMINE_VERSION "9.6.4"
 #define LIMINE_COPYRIGHT "Copyright (C) 2019-2025 Mintsuki and contributors."
 
 static void version_usage(void) {
