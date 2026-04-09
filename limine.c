3c16d4c3bb2bb54ecc8b37b71760fdb20a193caa
Mintsuki <mintsuki@protonmail.com>
2026-04-09 11:57:31 +0000

    Binary release v11.3.1

--- limine.c
+++ limine.c
@@ -1360,7 +1360,7 @@ cleanup:
     return ret;
 }
 
-#define LIMINE_VERSION "11.2.1"
+#define LIMINE_VERSION "11.3.1"
 #define LIMINE_COPYRIGHT "Copyright (C) 2019-2026 Mintsuki and contributors."
 
 static void version_usage(void) {
