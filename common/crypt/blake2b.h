d267f6b91e3da36e9c0753c2dccb7b54c2624f2c
Kamila Szewczyk <k@iczelia.net>
2026-04-13 00:46:59 +0200

    crypt: blake2b.h: opaque pointer to fs guts

--- common/crypt/blake2b.h
+++ common/crypt/blake2b.h
@@ -5,10 +5,9 @@
 
 #define BLAKE2B_OUT_BYTES 64
 
-#include <fs/file.h>
-
 void blake2b(void *out, const void *in, size_t in_len);
 
+struct file_handle;
 struct file_handle * blake2b_open(struct file_handle * source);
 bool blake2b_check_hash(struct file_handle *fd, void* reference_hash);
 
