83a085718b354031d9fd7cd12627cb89afe5cd64
Kamila Szewczyk <k@iczelia.net>
2026-06-20 20:53:46 +0200

    tools/limlzpack: style guide adherence.

--- tools/limlzpack.c
+++ tools/limlzpack.c
@@ -1,16 +1,16 @@
 /* limlz: Copyright (C) 2026 Kamila Szewczyk <k@iczelia.net>
  * limine: Copyright (C) 2019-2026 Mintsuki and contributors.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -22,7 +22,7 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
