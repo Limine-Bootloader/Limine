9f30fd24579441190a7e0e8255eb0d671fa19e2a
Eggchese <tunglamtrannguyen3@gmail.com>
2026-08-30 13:47:23 +0700

    Delete test directory

--- test/e9print.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdarg.h>
-#include <stddef.h>
-
-void putchar(char c);
-void print(const char *msg);
-void puts(const char *msg);
-void printf(const char *format, ...) __attribute__((format(printf, 1, 2)));
